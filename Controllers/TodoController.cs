6d2ea6dde8c7c14ca0c3371d9e79416dfb0c5c3e
Lactsoy <lihourcbd@gmail.com>
2026-04-14 01:18:40 +0700

    first working todo app

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TodoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Show all todos
+        public IActionResult Index()
+        {
+            var items = _context.TodoItems.ToList();
+            return View(items);
+        }
+
+        // Add new todo
+        [HttpPost]
+        public IActionResult Add(string title)
+        {
+            if (!string.IsNullOrEmpty(title))
+            {
+                _context.TodoItems.Add(new TodoItem { Title = title });
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        // Delete todo
+        public IActionResult Delete(int id)
+        {
+            var item = _context.TodoItems.Find(id);
+            if (item != null)
+            {
+                _context.TodoItems.Remove(item);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApp.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public bool IsDone { get; set; }
+    }
+}
